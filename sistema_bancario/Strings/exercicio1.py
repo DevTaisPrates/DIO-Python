dd8e870b89894c36589a90282d0c8de11f9c6483
Taís Prates <taisbezerra24777@gmail.com>
2025-09-30 11:42:15 -0300

    Delete sistema_bancario directory

--- sistema_bancario/Strings/exercicio1.py
+++
@@ -1,9 +0,0 @@
-mensagem = 'Alguns métodos de classe str.'
-
-print(mensagem.upper()) # Os caracteres da string fica maiúsculo.
-print(mensagem.lower()) # Os caracteres da string fica minúsculo.
-print(mensagem.title()) # Apenas o primeiro caractere da string fica maiúsculo, os demais ficam minúsculo.
-print(mensagem.format()) # Formata valores especificados em uma cadeia de caracteres
-print(mensagem.isalnum())  # Retorna True se todos os caracteres na string forem alfanuméricos
-print(mensagem.isalpha()) # Retorna True se todos os caracteres da cadeia de caracteres estiverem no alfabeto
-print(mensagem.isdecimal()) # Retorna True se todos os caracteres na string forem decimais
--- sistema_bancario/operadores/exercicio3.py
+++
@@ -1,7 +0,0 @@
-valor = int(input('Digite um valor: '))
-
-while valor % 2 == 0:
-    print('HAHAHA')
-    valor = int(input('Digite um valor: '))
-
-print('Número ímpar')
